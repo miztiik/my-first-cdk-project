cb09c9f0e6683e7c08bdce8188572ab98fae5a04
Mystique <13oct08@quantumfoam.uni.cc>
2020-02-14 00:26:31 +0100

    Initial Commit

--- my_first_cdk_project/my_first_cdk_project_stack.py
+++ my_first_cdk_project/my_first_cdk_project_stack.py
@@ -1,4 +1,7 @@
-from aws_cdk import core
+from aws_cdk import (
+    aws_s3 as _s3,
+    core
+)
 
 
 class MyFirstCdkProjectStack(core.Stack):
@@ -7,3 +10,11 @@ class MyFirstCdkProjectStack(core.Stack):
         super().__init__(scope, id, **kwargs)
 
         # The code that defines your stack goes here
+
+        _s3.Bucket(
+            self,
+            "myBucketId",
+            bucket_name="myfirstcdkproject010",
+            versioned=False,
+            encryption=_s3.BucketEncryption.KMS_MANAGED
+        )
