e2766fd8b8284df68cb95570a4d431e389990a2b
Mystique <13oct08@quantumfoam.uni.cc>
2020-02-15 21:07:30 +0100

    Tokens | Intrinsic Functions

--- my_first_cdk_project/my_first_cdk_project_stack.py
+++ my_first_cdk_project/my_first_cdk_project_stack.py
@@ -19,3 +19,23 @@ class MyFirstCdkProjectStack(core.Stack):
             encryption=_s3.BucketEncryption.S3_MANAGED,
             block_public_access=_s3.BlockPublicAccess.BLOCK_ALL
         )
+
+        mybucket = _s3.Bucket(
+            self,
+            "myBucketId1"
+        )
+
+        snstopicname = "abczys1234"
+
+        if not core.Token.is_unresolved(snstopicname) and len(snstopicname) > 10:
+            raise ValueError("Maximum value can be only 10 characters")
+
+        print(mybucket.bucket_name)
+
+        output_1 = core.CfnOutput(
+            self,
+            "myBucketOutput1",
+            value=mybucket.bucket_name,
+            description=f"My first CDK Bucket",
+            export_name="myBucketOutput1"
+        )
