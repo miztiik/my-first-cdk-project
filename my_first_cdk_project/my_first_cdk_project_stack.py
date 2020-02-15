9dd10b4b0f336a1a97b24203a07d0e9c4ff6e498
Mystique <13oct08@quantumfoam.uni.cc>
2020-02-16 00:49:43 +0100

    opt-out-telemetry

--- my_first_cdk_project/my_first_cdk_project_stack.py
+++ my_first_cdk_project/my_first_cdk_project_stack.py
@@ -11,27 +11,10 @@ class MyFirstCdkProjectStack(core.Stack):
         super().__init__(scope, id, **kwargs)
 
         # The code that defines your stack goes here
-
-        _s3.Bucket(
-            self,
-            "myBucketId",
-            bucket_name="myfirstcdkproject010",
-            versioned=False,
-            encryption=_s3.BucketEncryption.S3_MANAGED,
-            block_public_access=_s3.BlockPublicAccess.BLOCK_ALL
-        )
-
         mybucket = _s3.Bucket(
             self,
             "myBucketId1"
         )
-
-        snstopicname = "abczys1234"
-
-        if not core.Token.is_unresolved(snstopicname) and len(snstopicname) > 10:
-            raise ValueError("Maximum value can be only 10 characters")
-
-        print(mybucket.bucket_name)
 
         _iam.Group(self,
                    "gid")
