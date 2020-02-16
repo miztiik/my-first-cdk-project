3836c6c58135b5195ec0018a61ca24a7a8dbc213
Mystique <13oct08@quantumfoam.uni.cc>
2020-02-17 00:31:52 +0100

    parameters_context_variables

--- app.py
+++ app.py
@@ -4,9 +4,12 @@ from aws_cdk import core
 
 from my_first_cdk_project.my_first_cdk_project_stack import MyArtifactBucketStack
 
-env_US = core.Environment(account="904521749370", region="us-east-1")
-env_EU = core.Environment(account="313487964768", region="eu-west-1")
+
 app = core.App()
+env_US = core.Environment(account=app.node.try_get_context('dev')['account'],
+                          region=app.node.try_get_context('dev')['region'])
+env_EU = core.Environment(account=app.node.try_get_context('prod')['account'],
+                          region=app.node.try_get_context('prod')['region'])
 MyArtifactBucketStack(app, "myDevStack", env=env_US)
 MyArtifactBucketStack(app, "myProdStack", is_prod=True, env=env_EU)
 
--- my_first_cdk_project/my_first_cdk_project_stack.py
+++ my_first_cdk_project/my_first_cdk_project_stack.py
@@ -1,5 +1,6 @@
 from aws_cdk import (
     aws_s3 as _s3,
+    aws_kms as _kms,
     core
 )
 
@@ -11,11 +12,16 @@ class MyArtifactBucketStack(core.Stack):
 
         # The code that defines your stack goes here
 
+        mykey = _kms.Key.from_key_arn(self,
+                                      "myKeyId",
+                                      self.node.try_get_context('prod')['kms_arn'])
+
         if is_prod:
             artifactBucket = _s3.Bucket(self,
                                         "myProdArtifactBucketId",
                                         versioned=True,
-                                        encryption=_s3.BucketEncryption.S3_MANAGED,
+                                        encryption=_s3.BucketEncryption.KMS,
+                                        encryption_key=mykey,
                                         removal_policy=core.RemovalPolicy.RETAIN)
         else:
             artifactBucket = _s3.Bucket(self,
