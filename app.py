6ce08c6e0969577bb03b148862466c6e79c4247d
Mystique <13oct08@quantumfoam.uni.cc>
2020-02-24 10:27:51 +0100

    Build Multi-AZ Production Ready Custom VPC

--- app.py
+++ app.py
@@ -2,15 +2,11 @@
 
 from aws_cdk import core
 
-from my_first_cdk_project.my_first_cdk_project_stack import MyArtifactBucketStack
+from resource_stacks.custom_vpc import CustomVpcStack
 
 
 app = core.App()
-env_US = core.Environment(account=app.node.try_get_context('dev')['account'],
-                          region=app.node.try_get_context('dev')['region'])
-env_EU = core.Environment(account=app.node.try_get_context('prod')['account'],
-                          region=app.node.try_get_context('prod')['region'])
-MyArtifactBucketStack(app, "myDevStack", env=env_US)
-MyArtifactBucketStack(app, "myProdStack", is_prod=True, env=env_EU)
+
+CustomVpcStack(app, "my-custom-vpc-stack")
 
 app.synth()
---
+++ resource_stacks/custom_vpc.py
@@ -0,0 +1,34 @@
+from aws_cdk import aws_ec2 as _ec2
+from aws_cdk import core
+
+
+class CustomVpcStack(core.Stack):
+
+    def __init__(self, scope: core.Construct, id: str, ** kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        prod_configs = self.node.try_get_context('envs')['prod']
+
+        custom_vpc = _ec2.Vpc(
+            self,
+            "customVpcId",
+            cidr=prod_configs['vpc_configs']['vpc_cidr'],
+            max_azs=2,
+            nat_gateways=1,
+            subnet_configuration=[
+                _ec2.SubnetConfiguration(
+                    name="publicSubnet", cidr_mask=prod_configs['vpc_configs']['cidr_mask'], subnet_type=_ec2.SubnetType.PUBLIC
+                ),
+                _ec2.SubnetConfiguration(
+                    name="privateSubnet", cidr_mask=prod_configs['vpc_configs']['cidr_mask'], subnet_type=_ec2.SubnetType.PRIVATE
+                ),
+                _ec2.SubnetConfiguration(
+                    name="dbSubnet", cidr_mask=prod_configs['vpc_configs']['cidr_mask'], subnet_type=_ec2.SubnetType.ISOLATED
+                )
+            ]
+        )
+
+        core.CfnOutput(self,
+                       "customVpcOutput",
+                       value=custom_vpc.vpc_id,
+                       export_name="customVpcId")
