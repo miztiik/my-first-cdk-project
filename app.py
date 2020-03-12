61a545fd89e1b06c2dd066a5c9581e597f91ff3b
Mystique <13oct08@quantumfoam.uni.cc>
2020-03-12 16:25:36 +0100

    tagging_at_scale

--- app.py
+++ app.py
@@ -9,4 +9,9 @@ app = core.App()
 
 CustomVpcStack(app, "my-custom-vpc-stack")
 
+core.Tag.add(app, key="stack-name", value="network-stack")
+core.Tag.add(app, key="support-team",
+             value=app.node.try_get_context('envs')['prod']['support-email'])
+
+
 app.synth()
